abe6ac93713ad9d65d814aebb1680a1d77f42750
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-02-28 16:08:32 -0500

    Update category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -13,9 +13,16 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// GET a single category
+router.get("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post("/", (req, res) => {
