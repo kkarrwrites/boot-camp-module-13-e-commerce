d9192f01f8b746244d1162abae4ca48373e84f11
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-02-28 14:41:03 -0500

    Add associations

--- models/index.js
+++ models/index.js
@@ -3,13 +3,35 @@ const Category = require("./Category");
 const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
+// Product belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
-// Categories have many Products
+// Category has many Product
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Product belongToMany Tag (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "product_tags",
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Tag belongToMany Product (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "tag_products",
+});
 
 module.exports = {
   Product,
