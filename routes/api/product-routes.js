bdc2433b611e93b1cacfed840e047b2472abbf3a
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-02-28 15:43:57 -0500

    Update product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,7 +7,9 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 // TODO: Include associated Tag data
 router.get("/", async (req, res) => {
   try {
-    const productData = await Product.findAll({ include: Category });
+    const productData = await Product.findAll({
+      include: [{ model: Category }],
+    });
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
