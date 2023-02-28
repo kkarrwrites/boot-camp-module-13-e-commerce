046f7c584fa9920350fc270198e28dfe6b534029
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-02-28 15:43:51 -0500

    Update index.js

--- models/index.js
+++ models/index.js
@@ -21,7 +21,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false,
   },
-  as: "product_tags",
+  as: "product_id",
 });
 
 // Tag belongToMany Product (through ProductTag)
@@ -30,7 +30,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false,
   },
-  as: "tag_products",
+  as: "tag_id",
 });
 
 module.exports = {
