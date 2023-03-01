6dcd2781381235bddb154d21f625ca7e87351a28
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-03-01 14:46:54 -0500

    Update category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -25,16 +25,42 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
-  // create a new category
+// POST a new category
+router.post("/", async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put("/:id", (req, res) => {
-  // update a category by its `id` value
+// PUT (update) a category by its id value
+router.put("/:id", async (req, res) => {
+  const categoryData = await Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((categoryData) => {
+      res.status(200).json(categoryData);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
-router.delete("/:id", (req, res) => {
-  // delete a category by its `id` value
+// DELETE a category by its id value
+router.delete("/:id", async (req, res) => {
+  const categoryData = Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((categoryData) => res.status(200).json(categoryData))
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
